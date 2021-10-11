c3f01b98db01e6901a13bb75b89dbd494554bcd1
Madhushree Ganiga/Javacode-Wolken <92204525+Madhushree-ganiga@users.noreply.github.com>
2021-10-11 15:20:58 +0530

    Add files via upload

---
+++ Param.java
@@ -0,0 +1,11 @@
+public class Param {
+  static void myParam(String fname, int age) {
+    System.out.println(fname + " is " + age);
+  }
+
+  public static void main(String[] args) {
+    myParam("ganiga", 6);
+    myParam("Pratheeka", 21);
+    myParam("priya", 12);
+  }
+}
---
+++ RCB.java
@@ -0,0 +1,24 @@
+class RCB
+{
+static void players()
+{
+ System.out.println("shetty");
+ System.out.println("prathii");
+}
+
+static void players(String name)
+{
+ System.out.println(name);
+ System.out.println("MADHU");
+ System.out.println("PRIYA");
+}
+static void players(int num)
+{
+ 
+ System.out.println(num);
+ System.out.println(" MADHUSHREE");
+}
+static byte noBy(){
+	return 8;
+}
+}
